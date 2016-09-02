0b5bafc0424a10f7c680c1a1d425db8d2e083b0f
Remy <Remye98@gmail.com>
2016-09-02 12:04:22 +0200

    working program assignment 1.1

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,45 @@
  */
 public class Main {
 
-    public static void main(String[] args) {
+    private int randomNumbers;
+    int[] sortedNumbers = new int[25000] ;
 
-        System.out.println("hello remy");
+    public void BubbleSort(int[] numbers){
+        boolean flag = true;
+        int temp;
+
+        while(flag){
+            flag=false; //set flag to false awaiting a possible swap
+            for (int j = 0; j < sortedNumbers.length-1 ; j++) {
+                if(sortedNumbers[j]<sortedNumbers[j+1]){
+
+                    temp=sortedNumbers[j];      //swap elements
+                    sortedNumbers[j]=sortedNumbers[j+1];
+                    sortedNumbers[j+1]=temp;
+                    flag=true;      //shows a swap occurred
+                }
+            }
+        }
+    }
+
+    public void AddNumbersToList(){
+        for (int i = 0; i < sortedNumbers.length; i++) {
+            int j = RandomInt(25000)+1;
+            sortedNumbers[i]=j;
+        }
+        BubbleSort(sortedNumbers);
+        for (int i = 0; i < sortedNumbers.length; i++) {
+            System.out.println(sortedNumbers[i]);
+        }
+    }
+
+    public int RandomInt(int amount){
+        randomNumbers = (int)(Math.random() * amount);
+        return randomNumbers;
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.AddNumbersToList();
     }
 }
