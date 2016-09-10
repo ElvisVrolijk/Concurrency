71b8534f1ed63f5dd7d97ab40c41e33deec633f8
Remy <Remye98@gmail.com>
2016-09-10 13:44:26 +0200

    comments

--- src/Main.java
+++ src/Main.java
@@ -6,48 +6,51 @@ import java.util.Arrays;
  */
 public class Main {
 
-    private int[] sortedNumbers1;  //change these two to equal the tested number
+    private int[] sortedNumbers1;
     private int threshold;
 
     private int[] list1;
     private int[] list2;
 
+    //The bubble sort algorithm
     private void bubbleSort(int[] numbers) {
         boolean flag = true;
         int temp;
 
         while (flag) {
-            flag = false; //set flag to false awaiting a possible swap
+            flag = false; //Set flag to false awaiting a possible swap
             for (int j = 0; j < numbers.length - 1; j++) {
                 if (numbers[j] > numbers[j + 1]) {
 
-                    temp = numbers[j];      //swap elements
+                    temp = numbers[j];      //Swap elements
                     numbers[j] = numbers[j + 1];
                     numbers[j + 1] = temp;
-                    flag = true;      //shows a swap occurred
+                    flag = true;      //Shows a swap occurred
                 }
             }
         }
     }
 
+    //Add random numbers to the list that has to be sorted
     private void addNumbersToList() {
+        //add the generated numbers to the list
         for (int i = 0; i < sortedNumbers1.length; i++) {
             int j = RandomInt(sortedNumbers1.length) + 1;
             sortedNumbers1[i] = j;
         }
 
+        //Split the list in to halves
         for (int i = 0; i < sortedNumbers1.length; i++) {
             if (i < sortedNumbers1.length / 2) {
                 list1[i] = sortedNumbers1[i];
             } else {
                 list2[i - sortedNumbers1.length / 2] = sortedNumbers1[i];
             }
-
         }
     }
 
 
-
+    //Merge sort algorithm
     private int[] merge(int[] a, int[] b) {
         int[] answer = new int[a.length + b.length];
         int i = 0, j = 0, k = 0;
@@ -70,6 +73,7 @@ public class Main {
         return answer;
     }
 
+
     private class Sorter implements Runnable {
         private int[] array;
 
@@ -83,19 +87,19 @@ public class Main {
         }
     }
 
+    //if the array length is bigger than the threshold
+    //split the array in to two halves and create two new threads for the two new arrays
+    //else bubblesort the array
     private int[] usingThread(int[] array) {
         //System.out.println("BEGIN: " + Arrays.toString(array));
         if (array.length > threshold) {
             int[] array1 = Arrays.copyOfRange(array, 0, array.length / 2);
             int[] array2 = Arrays.copyOfRange(array, array1.length, array.length);
 
-
-            //Thread t1 = new Thread(() -> usingThread(array1));
             Sorter sorter1 = new Sorter(array1);
             Sorter sorter2 = new Sorter(array2);
             Thread t1 = new Thread(sorter1);
             Thread t2 = new Thread(sorter2);
-            //Thread t2 = new Thread(() -> usingThread(array2));
 
             t1.start();
             t2.start();
@@ -107,10 +111,9 @@ public class Main {
             }
 
             array = merge(sorter1.array, sorter2.array);
-//            return array;
 
         } else {
-           bubbleSort(array);
+            bubbleSort(array);
         }
         //System.out.println("END: " + Arrays.toString(array));
         return array;
@@ -123,19 +126,23 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
 
-        int[] arrayLength = new int[] {25000, 50000, 100000, 200000, 400000, 800000};
-        int[] thresholds = new int[] {500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000};
+        //All the value to be tested
 
-         for (int array : arrayLength) {
-             main.sortedNumbers1 = new int[array];
+        //The length of the unsorted array
+        int[] arrayLength = new int[]{25000, 50000, 100000, 200000, 400000, 800000};
+        //The thresholds
+        int[] thresholds = new int[]{500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000};
 
-             main.list1 = Arrays.copyOfRange(main.sortedNumbers1, 0, main.sortedNumbers1.length / 2);
-             main.list2 = Arrays.copyOfRange(main.sortedNumbers1, main.list1.length, main.sortedNumbers1.length);
+        for (int array : arrayLength) {
+            main.sortedNumbers1 = new int[array];
 
-             main.addNumbersToList();
+            main.list1 = Arrays.copyOfRange(main.sortedNumbers1, 0, main.sortedNumbers1.length / 2);
+            main.list2 = Arrays.copyOfRange(main.sortedNumbers1, main.list1.length, main.sortedNumbers1.length);
 
-             for (int holdNumber : thresholds) {
-                 main.threshold = holdNumber;
+            main.addNumbersToList();
+
+            for (int holdNumber : thresholds) {
+                main.threshold = holdNumber;
 
                 final long startTime = System.currentTimeMillis();      //start timer
 
